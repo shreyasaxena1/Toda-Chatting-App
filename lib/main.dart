dec576fb6b7c0d3387dab07c2ef7c2f6ccb00f75
shrey <shreyasaxena117@gmail.com>
2021-05-15 16:38:56 +0530

    Progress 1.3

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,12 @@ class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-     initialRoute: WelcomeScreen.id ,
+      initialRoute: WelcomeScreen.id,
       routes: {
-        WelcomeScreen.id : (context) => WelcomeScreen(),
+        WelcomeScreen.id: (context) => WelcomeScreen(),
         LoginScreen.id: (context) => LoginScreen(),
-        RegistrationScreen.id:(context)  => RegistrationScreen(),
-        ChatScreen.id: (context)  => ChatScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
       },
     );
   }
